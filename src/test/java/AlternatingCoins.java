08e0fa4dd9d1d2111705eca41697c621dd9740ff
sungyoungKwon85 <ssyang0@naver.com>
2020-04-06 13:55:20 +0900

    codility

--- src/test/java/AlternatingCoins.java
+++ src/test/java/AlternatingCoins.java
@@ -1,5 +1,81 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class AlternatingCoins {
+/*
+There are N coins, each showing either heads or tails. We would like all the coins to form a sequence of alternating heads and tails. What is the minimum number of coins that must be reversed to achieve this?
 
+Write a function:
+
+class Solution { public int solution(int[] A); }
+
+that, given an array A consisting of N integers representing the coins, returns the minimum number of coins that must be reversed. Consecutive elements of array A represent consecutive coins and contain either a 0 (heads) or a 1 (tails).
+
+
+Examples:
+
+1. Given array A = [1, 0, 1, 0, 1, 1], the function should return 1. After reversing the sixth coin, we achieve an alternating sequence of coins [1, 0, 1, 0, 1, 0].
+
+2. Given array A = [1, 1, 0, 1, 1], the function should return 2. After reversing the first and fifth coins, we achieve an alternating sequence [0, 1, 0, 1, 0].
+
+3. Given array A = [0, 1, 0], the function should return 0. The sequence of coins is already alternating.
+
+4. Given array A = [0, 1, 1, 0], the function should return 2. We can reverse the first and second coins to get the sequence: [1, 0, 1, 0].
+ */
+    @Test
+    public void test1() {
+        int solution = solution(new int[]{1, 0, 1, 0, 1, 1});
+        assertEquals(solution, 1);
+    }
+
+    @Test
+    public void test2() {
+        int solution = solution(new int[]{1,1,0,1,1});
+        assertEquals(solution, 2);
+    }
+
+    @Test
+    public void test3() {
+        int solution = solution(new int[]{1,0,1});
+        assertEquals(solution, 0);
+    }
+
+    @Test
+    public void test4() {
+        int solution = solution(new int[]{0,1,1,0});
+        assertEquals(solution, 2);
+    }
+
+    public int solution(int[] A) {
+        if (A.length == 1) {
+            return 0;
+        }
+
+        boolean prev1 = A[0] == 1 ? true : false;
+        int result1 = 0;
+
+        boolean prev2 = A[0] == 1 ? false : true;
+        int result2 = 1;
+
+        for (int i = 1; i < A.length; i++) {
+            boolean now = A[i] == 1 ? true : false;
+            if (prev1 == now) {
+                result1++;
+                prev1 = !prev1;
+            } else {
+                prev1 = now;
+            }
+            if (prev2 == now) {
+                result2++;
+                prev2 = !prev2;
+            } else {
+                prev2 = now;
+            }
+        }
+        return result1 > result2 ? result2 : result1;
+    }
 }
--- src/test/java/CheapLetterDeletion.java
+++ src/test/java/CheapLetterDeletion.java
@@ -1,5 +1,68 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class CheapLetterDeletion {
+/*
+You are given a string S. Deletion of the K-th letter of S costs C[K]. After deleting a letter, the costs of deleting other letters do not change. For example, for S = "ab" and C = [1, 3], after deleting 'a', deletion of 'b' will still cost 3.
+
+You want to delete some letters from S to obtain a string without two identical letters next to each other. What is the minimum total cost of deletions to achieve such a string?
+
+Write a function:
+
+class Solution { public int solution(String S, int[] C); }
+
+that, given string S and array C of integers, both of length N, returns the minimum cost of all necessary deletions.
+
+Examples:
+
+1. Given S = "abccbd" and C = [0, 1, 2, 3, 4, 5], the function should return 2. You can delete the first occurrence of 'c' to achieve "abcbd".
+
+2. Given S = "aabbcc" and C = [1, 2, 1, 2, 1, 2], the function should return 3. By deleting all letters with a cost of 1, you can achieve string "abc".
+
+3. Given S = "aaaa" and C = [3, 4, 5, 6], the function should return 12. You need to delete all but one letter 'a', and the lowest cost of deletions is 3+4+5=12.
+
+4. Given S = "ababa" and C = [10, 5, 10, 5, 10], the function should return 0. There is no need to delete any letter.
+ */
+    @Test
+    public void test1() {
+        int result = solution("abccbd", new int[]{0, 1, 2, 3, 4, 5});
+        assertEquals(result, 2);
+    }
+
+    @Test
+    public void test2() {
+        int result = solution("aabbcc", new int[]{1, 2, 1, 2, 1, 2});
+        assertEquals(result, 3);
+    }
+
+    @Test
+    public void test3() {
+        int result = solution("aaaa", new int[]{3, 4, 5, 6});
+        assertEquals(result, 12);
+    }
+
+    @Test
+    public void test4() {
+        int result = solution("ababa", new int[]{10, 5, 10, 5, 10});
+        assertEquals(result, 0);
+    }
+
+    public int solution(String S, int[] C) {
+        int result = 0;
+        char prev = S.charAt(0);
+        int prevIndex = 0;
+        for (int i = 1; i < S.length(); i++) {
+            char now = S.charAt(i);
+            if (prev == now) {
+                int less = C[prevIndex] < C[i] ? prevIndex : i;
+                result += C[less];
+            }
+            prevIndex = i;
+            prev = now;
+        }
+        return result;
+    }
 
 }
